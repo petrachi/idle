75fda1ac9c15def484f09085e31b1241edfc1ca8
Thomas Petrachi <tpetrachizago@gmail.com>
2016-09-08 00:19:27 +0200

    ch1-1 write

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.find_or_create_by(tag: :'ch1-1') do |a|
+  a.title = "Chapitre 1 - Première Partie"
+  a.group = :hdli
+  a.published = true
+end
+
+Article.find_or_create_by(tag: :'ch1-2') do |a|
+  a.title = "Chapitre 1 - Seconde Partie"
+  a.group = :hdli
+  a.published = true
+end
